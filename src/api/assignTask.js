7f5e3a9cc3cf4f72406a92028fad8e8089cd9538
Koyal Nimbalkar <koyalkakade@gmail.com>
2026-06-30 14:41:38 +0530

    Task Management

---
+++ src/api/assignTask.js
@@ -0,0 +1,17 @@
+import axiosInstance from "./axiosInstance";
+
+
+export const assignTaskToUserAPI = async (payload) => {
+  const res = await axiosInstance.post("/assign/assign-task", payload);
+  return res.data;
+};
+
+export const getTasksByUSer = async () => {
+  const res = await axiosInstance.get("/assign/get-tasks-by-user");
+  return res.data;
+};
+
+export const updateTaskStatus = async (id, data) => {
+    const res = await axiosInstance.patch(`/task/updateStatus/${id}`, data);
+    return res.data;
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { FaSignOutAlt, FaUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { getUserInfo } from "../api/api";
+
+const Navbar = () => {
+ const [user, setUser] = useState()
+  const navigate = useNavigate();
+
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+
+    navigate("/");
+  };
+
+  async function fetchData(){
+    const data = await getUserInfo()
+       const { looggedUser } = data;   // get looggedUser object
+        const { name } = looggedUser;
+    // setUser(data.loggedUser)
+    setUser(name)
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow">
+      <div className="container-fluid">
+        
+        {/* Left Side */}
+        <span className="navbar-brand fw-bold fs-4">
+          TaskMGMT
+        </span>
+
+        {/* Right Side */}
+        <div className="d-flex align-items-center gap-3 text-white">
+          <span>
+            <FaUserCircle size={22} className="me-2" />
+           {user} {/* {user?.name} */}
+          </span>
+
+          <button
+            className="btn btn-light btn-sm"
+            onClick={handleLogout}
+          >
+            <FaSignOutAlt className="me-1" />
+            Logout
+          </button>
+        </div>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
